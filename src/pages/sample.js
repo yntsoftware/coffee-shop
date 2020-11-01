f767f2ae78ea9994098115a1e037c4d99c9d371c
Nuey Thepyasuwan <nuey@yntsoftware.com>
2020-10-31 22:38:32 -0500

    Add initial configuration

---
+++ src/pages/sample.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './sample.module.css';
+
+export default function Sample() {
+  return (
+    <div>
+      <header className={styles.header}>Header</header>
+      <footer className={styles.footer}>Footer</footer>
+    </div>
+  )
+}
+

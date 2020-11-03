e617b24896420fe1d9121061c2587ae019ae5247
Nuey Thepyasuwan <nuey@yntsoftware.com>
2020-11-02 22:16:25 -0600

    Sort blog posts by date

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -6,7 +6,7 @@ import BlogPost from './BlogPost';
 export default function BlogList() {
   const data = useStaticQuery(graphql`
     {
-      allMarkdownRemark {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
           node {
             id 
